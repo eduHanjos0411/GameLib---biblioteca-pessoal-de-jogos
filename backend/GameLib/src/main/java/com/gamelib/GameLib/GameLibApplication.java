e31f1438b866ca135bb1f417138f736f166d5bbe
eduHanjos0411 <eduardo.h.anjos0411@gmail.com>
2026-08-24 10:46:25 -0300

    Initial commit backend

---
+++ backend/GameLib/src/main/java/com/gamelib/GameLib/GameLibApplication.java
@@ -0,0 +1,13 @@
+package com.gamelib.GameLib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameLibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameLibApplication.class, args);
+	}
+
+}
---
+++ backend/GameLib/src/test/java/com/gamelib/GameLib/GameLibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamelib.GameLib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameLibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
